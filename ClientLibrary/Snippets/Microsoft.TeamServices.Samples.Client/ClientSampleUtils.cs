80ebfe98abfe9648fbfe7cb96aba0b4f7aebedb5
Rahul Hasija <rahasija@microsoft.com>
2017-07-24 15:47:30 +0530

    retrieving all the bugs and there attributes

--- ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/ClientSampleUtils.cs
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/ClientSampleUtils.cs
@@ -16,164 +16,166 @@ using Microsoft.VisualStudio.Services.WebApi;
 namespace Microsoft.TeamServices.Samples.Client
 {
 
-    /// <summary>
-    /// Utilities for discovering client samples.
-    /// </summary>
-    public static class ClientSampleUtils
-    {
-        public static Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>> GetRunnableClientSampleMethods(string area = null, string resource = null)
-        {
-            Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>> results = new Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>>();
+	/// <summary>
+	/// Utilities for discovering client samples.
+	/// </summary>
+	public static class ClientSampleUtils
+	{
+		public static Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>> GetRunnableClientSampleMethods(string area = null, string resource = null)
+		{
+			Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>> results = new Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>>();
 
-            CompositionContainer container = new CompositionContainer(new AssemblyCatalog(Assembly.GetExecutingAssembly()));
+			CompositionContainer container = new CompositionContainer(new AssemblyCatalog(Assembly.GetExecutingAssembly()));
 
-            IEnumerable<Lazy<ClientSample>> samples = container.GetExports<ClientSample>();
-            foreach (Lazy<ClientSample> cs in samples)
-            {
-                try
-                {
-                    Type csType = cs.Value.GetType();
-                    ClientSampleAttribute csAttr = csType.GetCustomAttribute<ClientSampleAttribute>();
+			IEnumerable<Lazy<ClientSample>> samples = container.GetExports<ClientSample>();
+			foreach (Lazy<ClientSample> cs in samples)
+			{
+				try
+				{
+					Type csType = cs.Value.GetType();
+					ClientSampleAttribute csAttr = csType.GetCustomAttribute<ClientSampleAttribute>();
 
-                    List<RunnableClientSampleMethod> runnableMethods = new List<RunnableClientSampleMethod>();
+					List<RunnableClientSampleMethod> runnableMethods = new List<RunnableClientSampleMethod>();
 
-                    foreach (MethodInfo m in csType.GetMethods())
-                    {
-                        ClientSampleMethodAttribute[] attrs = (ClientSampleMethodAttribute[])m.GetCustomAttributes(typeof(ClientSampleMethodAttribute), false);
-                        foreach (var ma in attrs)
-                        {
-                            RunnableClientSampleMethod runnableMethod = new RunnableClientSampleMethod();
+					foreach (MethodInfo m in csType.GetMethods())
+					{
+						ClientSampleMethodAttribute[] attrs = (ClientSampleMethodAttribute[])m.GetCustomAttributes(typeof(ClientSampleMethodAttribute), false);
+						foreach (var ma in attrs)
+						{
+							RunnableClientSampleMethod runnableMethod = new RunnableClientSampleMethod();
 
-                            if (string.IsNullOrEmpty(ma.Area))
-                            {
-                                runnableMethod.Area = csAttr.Area;
-                            }
-                            else
-                            {
-                                runnableMethod.Area = ma.Area;
-                            }
+							if (string.IsNullOrEmpty(ma.Area))
+							{
+								runnableMethod.Area = csAttr.Area;
+							}
+							else
+							{
+								runnableMethod.Area = ma.Area;
+							}
 
-                            if (string.IsNullOrEmpty(ma.Resource))
-                            {
-                                runnableMethod.Resource = csAttr.Resource;
-                            }
-                            else
-                            {
-                                runnableMethod.Resource = ma.Resource;
-                            }
+							if (string.IsNullOrEmpty(ma.Resource))
+							{
+								runnableMethod.Resource = csAttr.Resource;
+							}
+							else
+							{
+								runnableMethod.Resource = ma.Resource;
+							}
 
-                            if (!string.IsNullOrEmpty(runnableMethod.Area) && !string.IsNullOrEmpty(runnableMethod.Resource))
-                            {
-                                runnableMethod.Area = char.ToUpper(runnableMethod.Area[0]) + runnableMethod.Area.Substring(1);
-                                runnableMethod.Resource = char.ToUpper(runnableMethod.Resource[0]) + runnableMethod.Resource.Substring(1);
+							if (!string.IsNullOrEmpty(runnableMethod.Area) && !string.IsNullOrEmpty(runnableMethod.Resource))
+							{
+								runnableMethod.Area = char.ToUpper(runnableMethod.Area[0]) + runnableMethod.Area.Substring(1);
+								runnableMethod.Resource = char.ToUpper(runnableMethod.Resource[0]) + runnableMethod.Resource.Substring(1);
 
-                                runnableMethod.MethodBase = m;
-                                runnableMethods.Add(runnableMethod);
-                            }
-                        }
-                    }
+								runnableMethod.MethodBase = m;
+								runnableMethods.Add(runnableMethod);
+							}
+						}
+					}
 
-                    if (runnableMethods.Any())
-                    {
-                        if (!String.IsNullOrEmpty(area))
-                        {
-                            runnableMethods = runnableMethods.FindAll(
-                                rcsm =>
-                                {
-                                    return string.Equals(area, rcsm.Area, StringComparison.InvariantCultureIgnoreCase) &&
-                                        (resource == null || string.Equals(resource, rcsm.Resource, StringComparison.InvariantCultureIgnoreCase));
-                                }
-                            );
-                        }
+					if (runnableMethods.Any())
+					{
+						if (!String.IsNullOrEmpty(area))
+						{
+							runnableMethods = runnableMethods.FindAll(
+								rcsm =>
+								{
+									return string.Equals(area, rcsm.Area, StringComparison.InvariantCultureIgnoreCase) &&
+										(resource == null || string.Equals(resource, rcsm.Resource, StringComparison.InvariantCultureIgnoreCase));
+								}
+							);
+						}
 
-                        results.Add(cs.Value, runnableMethods);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                }
-            }
+						results.Add(cs.Value, runnableMethods);
+					}
+				}
+				catch (Exception ex)
+				{
+					System.Diagnostics.Debug.WriteLine(ex.Message);
+				}
+			}
 
-            return results;
-        }
+			return results;
+		}
 
-        public static void RunClientSampleMethods(Uri connectionUrl, VssCredentials credentials, string area = null, string resource = null, DirectoryInfo outputPath = null)
-        {
-            if (area == "*")
-            {
-                area = null;
-            }
+		public static void RunClientSampleMethods(Uri connectionUrl, VssCredentials credentials, string area = null, string resource = null, DirectoryInfo outputPath = null)
+		{
+			if (area == "*")
+			{
+				area = null;
+			}
 
-            if (resource == "*")
-            {
-                resource = null;
-            }
+			if (resource == "*")
+			{
+				resource = null;
+			}
 
-            Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>> runnableMethodsBySample = GetRunnableClientSampleMethods(area, resource);
+			Dictionary<ClientSample, IEnumerable<RunnableClientSampleMethod>> runnableMethodsBySample = GetRunnableClientSampleMethods(area, resource);
 
-            if (runnableMethodsBySample.Any())
-            {
-                ClientSampleContext context = new ClientSampleContext(connectionUrl, credentials);
+			if (runnableMethodsBySample.Any())
+			{
+				ClientSampleContext context = new ClientSampleContext(connectionUrl, credentials);
 
-                Console.WriteLine("Start running client samples...");
-                Console.WriteLine("");
-                Console.WriteLine("  URL     : {0}", connectionUrl);
-                Console.WriteLine("  Area    : {0}", (area  == null ? "(all)" : area));
-                Console.WriteLine("  Resource: {0}", (resource == null ? "(all)" : resource));
-                Console.WriteLine("  Output  : {0}", (outputPath == null ? "(disabled)" : outputPath.FullName));
-                Console.WriteLine("");
+				Console.WriteLine("Start running client samples...");
+				Console.WriteLine("");
+				Console.WriteLine("  URL     : {0}", connectionUrl);
+				Console.WriteLine("  Area    : {0}", (area == null ? "(all)" : area));
+				Console.WriteLine("  Resource: {0}", (resource == null ? "(all)" : resource));
+				Console.WriteLine("  Output  : {0}", (outputPath == null ? "(disabled)" : outputPath.FullName));
+				Console.WriteLine("");
 
-                // Make sure we can connect before running the samples
-                context.Connection.ConnectAsync().SyncResult();
+				// Make sure we can connect before running the samples
+				context.Connection.ConnectAsync().SyncResult();
 
-                context.SetValue<DirectoryInfo>(ClientSampleHttpLogger.PropertyOutputFilePath, outputPath);
+				context.SetValue<DirectoryInfo>(ClientSampleHttpLogger.PropertyOutputFilePath, outputPath);
 
-                foreach (var item in runnableMethodsBySample)
-                {
-                    ClientSample clientSample = item.Key;
-                    clientSample.Context = context;
+				var item = runnableMethodsBySample.ElementAt(11);
 
-                    foreach (var runnableMethod in item.Value)
-                    {
-                        try
-                        {
-                            context.Log("+------------------------------------------------------------------------------+");
-                            context.Log("| {0} |", String.Format("{0}/{1}", runnableMethod.MethodBase.Name, runnableMethod.MethodBase.DeclaringType.Name).PadRight(76));
-                            context.Log("|                                                                              |");
-                            context.Log("| API: {0} |", String.Format("{0}/{1}", runnableMethod.Area, runnableMethod.Resource).PadRight(71));
-                            context.Log("+------------------------------------------------------------------------------+");
-                            context.Log("");
+				ClientSample clientSample = item.Key;
+				clientSample.Context = context;
 
-                            // Set these so the HTTP logger has access to them when it needs to write the output
-                            ClientSampleContext.CurrentRunnableMethod = runnableMethod;
-                            ClientSampleContext.CurrentContext = context;
+				var runnableMethod = item.Value.ElementAt(3);
+				var secondRunnableMethod = item.Value.ElementAt(4);
 
-                            // Reset suppression and operation name (don't want these values carrying over to the next sample method call)
-                            ClientSampleHttpLogger.SetSuppressOutput(context, false);
-                            ClientSampleHttpLogger.ResetOperationName(context);
+				try
+				{
+					context.Log("+------------------------------------------------------------------------------+");
+					context.Log("| {0} |", String.Format("{0}/{1}", runnableMethod.MethodBase.Name, runnableMethod.MethodBase.DeclaringType.Name).PadRight(76));
+					context.Log("|                                                                              |");
+					context.Log("| API: {0} |", String.Format("{0}/{1}", runnableMethod.Area, runnableMethod.Resource).PadRight(71));
+					context.Log("+------------------------------------------------------------------------------+");
+					context.Log("");
 
-                            runnableMethod.MethodBase.Invoke(clientSample, null);
-                        }
-                        catch (Exception ex)
-                        {
-                            //the innermost exception is the interesting one
-                            while (ex.InnerException != null) ex = ex.InnerException;
-                            Console.WriteLine("FAILED! With exception: " + ex.Message);
-                        }
-                        finally
-                        {
-                            context.Log("");
-                        }
-                    }
-                }
-            }
-        }
-    }
+					// Set these so the HTTP logger has access to them when it needs to write the output
+					ClientSampleContext.CurrentRunnableMethod = runnableMethod;
+					ClientSampleContext.CurrentContext = context;
 
-    public class RunnableClientSampleMethod : ClientSampleMethodInfo
-    {
-        public MethodBase MethodBase { get; set; }
-    }
+					// Reset suppression and operation name (don't want these values carrying over to the next sample method call)
+					ClientSampleHttpLogger.SetSuppressOutput(context, false);
+					ClientSampleHttpLogger.ResetOperationName(context);
+
+					runnableMethod.MethodBase.Invoke(clientSample, null);
+					secondRunnableMethod.MethodBase.Invoke(clientSample, null);
+				}
+				catch (Exception ex)
+				{
+					//the innermost exception is the interesting one
+					while (ex.InnerException != null) ex = ex.InnerException;
+					Console.WriteLine("FAILED! With exception: " + ex.Message);
+				}
+				finally
+				{
+					context.Log("");
+				}
+
+
+			}
+		}
+	}
+
+	public class RunnableClientSampleMethod : ClientSampleMethodInfo
+	{
+		public MethodBase MethodBase { get; set; }
+	}
 
 }
